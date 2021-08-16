49ac809c299a6ac0add638071ecee2f9c00855c4
Phil <pso@avwatch.us>
2021-08-16 16:11:29 -0400

    Mostly just playing around, but set up some stuff to allow the front end to communicate with the back end and vice versa

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from json import *
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 app.secret_key = 'vnkdjnfjknfl1232#'
@@ -6,6 +7,32 @@ app.secret_key = 'vnkdjnfjknfl1232#'
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/admin')
+def admin():
+    admin = True
+    return render_template('admin.html', admin = admin)
+
+@app.route('/notadmin')
+def notadmin():
+    admin = False
+    return render_template('admin.html', admin = admin)
+
+@app.route('/ajax_receive', methods=["GET", "POST"])
+def ajax_receive():
+    try:
+        data = request.data
+        string = str(data, "UTF-8")
+        json = loads(string)
+        # Here we'll have a handler function that takes json and unpacks it according to its command.
+        # that handler function will then send some response back that gets returned here.
+        # The app might not actually be complex enough to warrant this, but I wanted to practice
+        # setting up an ajax sender/receiver/handler framework
+        print(json)
+        dump = dumps(json)
+        return dumps(dump)
+    except ValueError:
+        return render_template('error.html')
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
--- static/js/test.js
+++ static/js/test.js
@@ -2,3 +2,24 @@ function test() {
     alert("javascript is hooked up")
     $(".testclass").html("and jquery is hooked up")
 }
+
+function ajaxtest(){
+    
+    let data = {
+        "command" : "this is a test",
+        "data" : "test data to be filled by whatever function is sending the request"
+    }
+
+    message_request = JSON.stringify( data )
+
+    $.ajax({
+        url:'/ajax_receive',
+        type: 'POST',
+        dataType: 'json',
+        data: message_request,
+        contentType:"application/json",
+        success: function(response){
+            alert(response)
+        }
+    })
+}
